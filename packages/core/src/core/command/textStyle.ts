95a64e3860b28e8a1f0ffe33d48399347b0847e5
luoluoqixi <luoqixi@luoqixi.com>
2025-09-02 23:14:11 +0800

    feat: has selection text style commands

--- packages/core/src/core/command/textStyle.ts
+++ packages/core/src/core/command/textStyle.ts
@@ -1,5 +1,5 @@
 import { syntaxTree } from '@codemirror/language';
-import { EditorSelection, StateCommand } from '@codemirror/state';
+import { EditorSelection, EditorState, StateCommand } from '@codemirror/state';
 import { Tree } from '@lezer/common';
 
 import { iterParentNodes, iterSubNodes } from '../utils';
@@ -146,12 +146,13 @@ const removeRangeMarkups = (
   return lineText;
 };
 
-const isCharInMarkup = (tree: Tree, pos: number, nodeType: string | string[]) => {
+const isCharInMarkup = (tree: Tree, from: number, nodeType: string | string[], to?: number) => {
+  to = to == null ? from + 1 : to;
   const isArray = Array.isArray(nodeType);
   let charInMarkup = false;
   tree.iterate({
-    from: pos,
-    to: pos + 1,
+    from,
+    to,
     enter: (node) => {
       if (isArray) {
         if (nodeType.includes(node.type.name as TextStyleNodeType)) {
@@ -184,107 +185,107 @@ const toggleTextStyle =
     const nodeType = config.nodeType;
     const tree = syntaxTree(state);
 
-    selection.ranges.forEach((range) => {
-      if (range.empty) {
-        const pos = range.from;
-        const charInMarkup = isCharInMarkup(tree, pos, nodeType);
-        if (charInMarkup) {
-          const updateChanges: UpdataChange[] = [];
-          const rangeCalculator = new SelectionRangeCalculator(pos, pos);
-          removeRangeMarkups(tree, pos, pos + 1, config, rangeCalculator, updateChanges);
-          changes.push(...updateChanges);
-          const to = rangeCalculator.getRange().to;
-          newRanges.push({
-            from: to,
-            to,
-          });
-        } else {
-          const insertText = `${config.markup}${config.placeholder || ''}${config.markup}`;
-          changes.push({
-            from: range.from,
-            to: range.to,
-            insert: insertText,
-          });
-          const cursorPos = range.from + config.markup.length;
-          newRanges.push({
-            from: cursorPos,
-            to: cursorPos + (config.placeholder?.length || 0),
-          });
-        }
+    const range = selection.main;
+
+    if (range.empty) {
+      const pos = range.from;
+      const charInMarkup = isCharInMarkup(tree, pos, nodeType);
+      if (charInMarkup) {
+        const updateChanges: UpdataChange[] = [];
+        const rangeCalculator = new SelectionRangeCalculator(pos, pos);
+        removeRangeMarkups(tree, pos, pos + 1, config, rangeCalculator, updateChanges);
+        changes.push(...updateChanges);
+        const to = rangeCalculator.getRange().to;
+        newRanges.push({
+          from: to,
+          to,
+        });
       } else {
-        const fromLine = doc.lineAt(range.from);
-        const toLine = doc.lineAt(range.to);
+        const insertText = `${config.markup}${config.placeholder || ''}${config.markup}`;
+        changes.push({
+          from: range.from,
+          to: range.to,
+          insert: insertText,
+        });
+        const cursorPos = range.from + config.markup.length;
+        newRanges.push({
+          from: cursorPos,
+          to: cursorPos + (config.placeholder?.length || 0),
+        });
+      }
+    } else {
+      const fromLine = doc.lineAt(range.from);
+      const toLine = doc.lineAt(range.to);
 
-        let notInAllMarkup = false;
-        const linesToProcess = [];
-        for (let i = fromLine.number; i <= toLine.number; i++) {
-          const line = doc.line(i);
-          const lineStart = Math.max(range.from, line.from);
-          const lineEnd = Math.min(range.to, line.to);
-          if (lineStart < lineEnd) {
-            let allMarkup = true;
+      let notInAllMarkup = false;
+      const linesToProcess = [];
+      for (let i = fromLine.number; i <= toLine.number; i++) {
+        const line = doc.line(i);
+        const lineStart = Math.max(range.from, line.from);
+        const lineEnd = Math.min(range.to, line.to);
+        if (lineStart < lineEnd) {
+          let allMarkup = true;
 
-            for (let pos = lineStart; pos < lineEnd; pos++) {
-              if (!isCharInMarkup(tree, pos, nodeType)) {
-                allMarkup = false;
-                break;
-              }
-            }
-
-            linesToProcess.push({
-              from: lineStart,
-              to: lineEnd,
-              allMarkup,
-            });
-            if (!allMarkup) {
-              notInAllMarkup = true;
+          for (let pos = lineStart; pos < lineEnd; pos++) {
+            if (!isCharInMarkup(tree, pos, nodeType)) {
+              allMarkup = false;
+              break;
             }
           }
+
+          linesToProcess.push({
+            from: lineStart,
+            to: lineEnd,
+            allMarkup,
+          });
+          if (!allMarkup) {
+            notInAllMarkup = true;
+          }
         }
+      }
 
-        const targetAddMarkup = notInAllMarkup;
-        const updateChanges: UpdataChange[] = [];
-        const rangeCalculator = new SelectionRangeCalculator(range.from, range.to);
+      const targetAddMarkup = notInAllMarkup;
+      const updateChanges: UpdataChange[] = [];
+      const rangeCalculator = new SelectionRangeCalculator(range.from, range.to);
 
-        const lineCount = linesToProcess.length;
-        linesToProcess.forEach((lineProcess, index) => {
-          const { from, to } = lineProcess;
-          let lineText = doc.sliceString(from, to);
-          let deleteCount = 0;
+      const lineCount = linesToProcess.length;
+      linesToProcess.forEach((lineProcess, index) => {
+        const { from, to } = lineProcess;
+        let lineText = doc.sliceString(from, to);
+        let deleteCount = 0;
 
-          // remove markup
-          const newLineText = removeRangeMarkups(
-            tree,
+        // remove markup
+        const newLineText = removeRangeMarkups(
+          tree,
+          from,
+          to,
+          config,
+          rangeCalculator,
+          updateChanges,
+          deleteCount,
+          lineText,
+        )!;
+        deleteCount += lineText.length - newLineText.length;
+        lineText = newLineText;
+
+        if (targetAddMarkup) {
+          // add markup
+          const newText = `${config.markup}${lineText}${config.markup}`;
+          rangeCalculator.addInsertion(from, config.markup.length);
+          if (index < lineCount - 1) {
+            rangeCalculator.addInsertion(to, config.markup.length);
+          }
+          updateChanges.push({
             from,
             to,
-            config,
-            rangeCalculator,
-            updateChanges,
-            deleteCount,
-            lineText,
-          )!;
-          deleteCount += lineText.length - newLineText.length;
-          lineText = newLineText;
+            insert: newText,
+          });
+        }
+      });
 
-          if (targetAddMarkup) {
-            // add markup
-            const newText = `${config.markup}${lineText}${config.markup}`;
-            rangeCalculator.addInsertion(from, config.markup.length);
-            if (index < lineCount - 1) {
-              rangeCalculator.addInsertion(to, config.markup.length);
-            }
-            updateChanges.push({
-              from,
-              to,
-              insert: newText,
-            });
-          }
-        });
-
-        changes.push(...updateChanges);
-        newRanges.push(rangeCalculator.getRange());
-      }
-    });
+      changes.push(...updateChanges);
+      newRanges.push(rangeCalculator.getRange());
+    }
 
     if (changes.length > 0) {
       const transaction = state.update({
@@ -308,71 +309,70 @@ const clearTextStyle = (nodeTypes: TextStyleNodeType[]): StateCommand => {
     const newRanges: { from: number; to: number }[] = [];
     const tree = syntaxTree(state);
 
-    selection.ranges.forEach((range) => {
-      if (range.empty) {
-        const pos = range.from;
-        const charInMarkup = isCharInMarkup(tree, pos, nodeTypes);
-        if (charInMarkup) {
-          const updateChanges: UpdataChange[] = [];
-          const rangeCalculator = new SelectionRangeCalculator(pos, pos);
-          for (let i = 0; i < nodeTypes.length; i++) {
-            const nodeType = nodeTypes[i];
-            const config = textStyleConfig[nodeType];
-            removeRangeMarkups(tree, pos, pos + 1, config, rangeCalculator, updateChanges);
-          }
-          changes.push(...updateChanges);
-          const to = rangeCalculator.getRange().to;
-          newRanges.push({
-            from: to,
-            to,
+    const range = selection.main;
+    if (range.empty) {
+      const pos = range.from;
+      const charInMarkup = isCharInMarkup(tree, pos, nodeTypes);
+      if (charInMarkup) {
+        const updateChanges: UpdataChange[] = [];
+        const rangeCalculator = new SelectionRangeCalculator(pos, pos);
+        for (let i = 0; i < nodeTypes.length; i++) {
+          const nodeType = nodeTypes[i];
+          const config = textStyleConfig[nodeType];
+          removeRangeMarkups(tree, pos, pos + 1, config, rangeCalculator, updateChanges);
+        }
+        changes.push(...updateChanges);
+        const to = rangeCalculator.getRange().to;
+        newRanges.push({
+          from: to,
+          to,
+        });
+      }
+    } else {
+      const fromLine = doc.lineAt(range.from);
+      const toLine = doc.lineAt(range.to);
+
+      const linesToProcess = [];
+      for (let i = fromLine.number; i <= toLine.number; i++) {
+        const line = doc.line(i);
+        const lineStart = Math.max(range.from, line.from);
+        const lineEnd = Math.min(range.to, line.to);
+        if (lineStart < lineEnd) {
+          linesToProcess.push({
+            from: lineStart,
+            to: lineEnd,
           });
         }
-      } else {
-        const fromLine = doc.lineAt(range.from);
-        const toLine = doc.lineAt(range.to);
-
-        const linesToProcess = [];
-        for (let i = fromLine.number; i <= toLine.number; i++) {
-          const line = doc.line(i);
-          const lineStart = Math.max(range.from, line.from);
-          const lineEnd = Math.min(range.to, line.to);
-          if (lineStart < lineEnd) {
-            linesToProcess.push({
-              from: lineStart,
-              to: lineEnd,
-            });
-          }
-        }
-
-        const updateChanges: UpdataChange[] = [];
-        const rangeCalculator = new SelectionRangeCalculator(range.from, range.to);
-
-        linesToProcess.forEach((lineProcess) => {
-          const { from, to } = lineProcess;
-          const lineText = doc.sliceString(from, to);
-          let deleteCount = 0;
-
-          for (let i = 0; i < nodeTypes.length; i++) {
-            const nodeType = nodeTypes[i];
-            const config = textStyleConfig[nodeType];
-            const newLineText = removeRangeMarkups(
-              tree,
-              from,
-              to,
-              config,
-              rangeCalculator,
-              updateChanges,
-              deleteCount,
-              lineText,
-            )!;
-            deleteCount += lineText.length - newLineText.length;
-          }
-        });
-
-        changes.push(...updateChanges);
-        newRanges.push(rangeCalculator.getRange());
       }
-    });
+
+      const updateChanges: UpdataChange[] = [];
+      const rangeCalculator = new SelectionRangeCalculator(range.from, range.to);
+
+      linesToProcess.forEach((lineProcess) => {
+        const { from, to } = lineProcess;
+        const lineText = doc.sliceString(from, to);
+        let deleteCount = 0;
+
+        for (let i = 0; i < nodeTypes.length; i++) {
+          const nodeType = nodeTypes[i];
+          const config = textStyleConfig[nodeType];
+          const newLineText = removeRangeMarkups(
+            tree,
+            from,
+            to,
+            config,
+            rangeCalculator,
+            updateChanges,
+            deleteCount,
+            lineText,
+          )!;
+          deleteCount += lineText.length - newLineText.length;
+        }
+      });
+
+      changes.push(...updateChanges);
+      newRanges.push(rangeCalculator.getRange());
+    }
 
     if (changes.length > 0) {
       const transaction = state.update({
@@ -388,6 +388,62 @@ const clearTextStyle = (nodeTypes: TextStyleNodeType[]): StateCommand => {
 
     return false;
   };
+};
+
+const hasTextStyle = (
+  state: EditorState,
+  allRange: boolean,
+  nodeTypes: TextStyleNodeType[],
+): boolean => {
+  const { selection, doc } = state;
+  let has = true;
+  const tree = syntaxTree(state);
+  const range = selection.main;
+  if (range.empty) {
+    const pos = range.from;
+    const charInMarkup = isCharInMarkup(tree, pos, nodeTypes);
+    if (!charInMarkup) {
+      has = false;
+    }
+  } else {
+    const fromLine = doc.lineAt(range.from);
+    const toLine = doc.lineAt(range.to);
+    let notInAllMarkup = false;
+    for (let i = fromLine.number; i <= toLine.number; i++) {
+      const line = doc.line(i);
+      const lineStart = Math.max(range.from, line.from);
+      const lineEnd = Math.min(range.to, line.to);
+      if (lineStart < lineEnd) {
+        if (allRange) {
+          let allMarkup = true;
+
+          for (let pos = lineStart; pos < lineEnd; pos++) {
+            if (!isCharInMarkup(tree, pos, nodeTypes)) {
+              allMarkup = false;
+              break;
+            }
+          }
+
+          if (!allMarkup) {
+            notInAllMarkup = true;
+            break;
+          }
+        } else {
+          if (lineStart < lineEnd) {
+            const charInMarkup = isCharInMarkup(tree, lineStart, nodeTypes, lineEnd);
+            if (!charInMarkup) {
+              notInAllMarkup = true;
+              break;
+            }
+          }
+        }
+      }
+    }
+    if (notInAllMarkup) {
+      has = false;
+    }
+  }
+  return has;
 };
 
 /**
@@ -414,6 +470,17 @@ export const clearSelectionHighlight = clearTextStyle(['Highlight']);
  * Clear specific text style in selection
  */
 export const clearSelectionInlineCode = clearTextStyle(['InlineCode']);
+
+/**
+ * Clear text style command map
+ */
+export const clearSelectionTextStyle: Record<TextStyleNodeType, StateCommand> = {
+  StrongEmphasis: clearSelectionStrong,
+  Emphasis: clearSelectionItalic,
+  Strikethrough: clearSelectionStrikethrough,
+  Highlight: clearSelectionHighlight,
+  InlineCode: clearSelectionInlineCode,
+};
 
 /**
  * Toggle strong emphasis command
@@ -448,12 +515,60 @@ export const toggleSelectionTextStyle: Record<TextStyleNodeType, StateCommand> =
 };
 
 /**
- * Clear text style command map
+ * Check if selection has any text style
+ * @param state Editor state
+ * @returns has any text style
  */
-export const clearSelectionTextStyle: Record<TextStyleNodeType, StateCommand> = {
-  StrongEmphasis: clearSelectionStrong,
-  Emphasis: clearSelectionItalic,
-  Strikethrough: clearSelectionStrikethrough,
-  Highlight: clearSelectionHighlight,
-  InlineCode: clearSelectionInlineCode,
+export const hasSelectionAnyTextStyle = (state: EditorState) => {
+  return hasTextStyle(state, true, [...textStyleNodeTypes]);
+};
+/**
+ * Check if selection has strong emphasis
+ * @param state Editor state
+ * @returns has strong emphasis
+ */
+export const hasSelectionStrongEmphasis = (state: EditorState) => {
+  return hasTextStyle(state, true, ['StrongEmphasis']);
+};
+/**
+ * Check if selection has emphasis
+ * @param state Editor state
+ * @returns has emphasis
+ */
+export const hasSelectionEmphasis = (state: EditorState) => {
+  return hasTextStyle(state, true, ['Emphasis']);
+};
+/**
+ * Check if selection has strikethrough
+ * @param state Editor state
+ * @returns has strikethrough
+ */
+export const hasSelectionStrikethrough = (state: EditorState) => {
+  return hasTextStyle(state, true, ['Strikethrough']);
+};
+/**
+ * Check if selection has highlight
+ * @param state Editor state
+ * @returns has highlight
+ */
+export const hasSelectionHighlight = (state: EditorState) => {
+  return hasTextStyle(state, true, ['Highlight']);
+};
+/**
+ * Check if selection has inline code
+ * @param state Editor state
+ * @returns has inline code
+ */
+export const hasSelectionInlineCode = (state: EditorState) => {
+  return hasTextStyle(state, true, ['InlineCode']);
+};
+/**
+ * Check if selection has specific text style
+ */
+export const hasSelectionTextStyle: Record<TextStyleNodeType, (state: EditorState) => boolean> = {
+  StrongEmphasis: hasSelectionStrongEmphasis,
+  Emphasis: hasSelectionEmphasis,
+  Strikethrough: hasSelectionStrikethrough,
+  Highlight: hasSelectionHighlight,
+  InlineCode: hasSelectionInlineCode,
 };
